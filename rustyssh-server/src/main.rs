ea818a954be62b8d1d1ed8b73e4730a55f491e26
benf3632 <benf3632@gmail.com>
2023-04-08 23:59:16 +0300

    better usage of mio

--- rustyssh-server/src/main.rs
+++ rustyssh-server/src/main.rs
@@ -4,9 +4,7 @@ use std::{io::Write, net::SocketAddr};
 
 use rustyssh::sshbuffer::SSHBuffer;
 
-const READABLE: Token = Token(0);
-const WRITEABLE: Token = Token(1);
-const READWRITEABLE: Token = Token(2);
+const MAIN: Token = Token(0);
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let addr = "127.0.0.1:7878".parse()?;
@@ -15,46 +13,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for socket in listener.incoming() {
         let socket = socket.unwrap();
         let addr = socket.peer_addr().unwrap();
+
+        socket.set_nonblocking(true).unwrap();
+
+        let socket = TcpStream::from_std(socket);
         println!("Accepted client");
         std::thread::spawn(move || handle_connection(socket, addr));
     }
     Ok(())
 }
 
-fn handle_connection(mut socket: std::net::TcpStream, addr: SocketAddr) {
+fn handle_connection(mut socket: TcpStream, addr: SocketAddr) {
     send_identifcation(&mut socket);
-
-    socket.set_nonblocking(true).unwrap();
-
-    let mut socket = TcpStream::from_std(socket);
 
     let mut poll = Poll::new().unwrap();
 
     let mut events = Events::with_capacity(128);
 
     poll.registry()
-        .register(
-            &mut socket,
-            READWRITEABLE,
-            Interest::READABLE | Interest::WRITABLE,
-        )
+        .register(&mut socket, MAIN, Interest::READABLE | Interest::WRITABLE)
         .unwrap();
 
     loop {
         poll.poll(&mut events, None).unwrap();
 
-        for event in events.iter() {
-            match event.token() {
-                READABLE => {}
-                WRITEABLE => {}
-                READWRITEABLE => {}
-                _ => unreachable!(),
-            }
-        }
+        for event in events.iter() {}
     }
 }
 
-fn send_identifcation(socket: &mut std::net::TcpStream) {
+fn send_identifcation(socket: &mut TcpStream) {
     let ident = format!("SSH-2.0-rustyssh{}\r\n", env!("CARGO_PKG_VERSION"));
     socket.write_all(ident.as_bytes()).unwrap();
 }
