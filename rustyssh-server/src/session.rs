5eb6b124a4abb05063498f88e81c2423d580a7f1
benf3632 <benf3632@gmail.com>
2023-04-11 18:04:52 +0300

    fix session usage

--- rustyssh-server/src/session.rs
+++ rustyssh-server/src/session.rs
@@ -1,9 +1,9 @@
 use std::net::SocketAddr;
 
-use rustyssh::session::{send_identification, Session};
+use rustyssh::session::Session;
 
 pub fn handle_connection(mut socket: mio::net::TcpStream, addr: SocketAddr) {
     let mut sess = Session::new(socket, addr);
-    send_identification(&mut sess);
+    sess.send_session_identification();
     sess.session_loop();
 }
