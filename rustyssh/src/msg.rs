fb24f8e901cb49fecc0310fde84e79895a3848ed
benf3632 <benf3632@gmail.com>
2023-04-12 20:49:12 +0300

    add packet types

--- rustyssh/src/msg.rs
+++ rustyssh/src/msg.rs
@@ -2,4 +2,9 @@ pub enum SSHMsg {
     // transport layer protocol messages refer to https://www.rfc-editor.org/rfc/rfc4253
     DISCONNECT = 1,
     IGNORE = 2,
+    UNIMPLEMENTED = 3,
+
+    // alogrithem negotiation
+    KEXINIT = 20,
+    NEWKEYS = 21,
 }
