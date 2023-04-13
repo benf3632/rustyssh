f51cd313c04e2907a02ffcb8eb1293dca5934c0e
benf3632 <benf3632@gmail.com>
2023-04-14 01:02:24 +0300

    add passing is_server

--- rustyssh-server/src/session.rs
+++ rustyssh-server/src/session.rs
@@ -3,7 +3,7 @@ use std::net::SocketAddr;
 use rustyssh::session::SessionHandler;
 
 pub fn handle_connection(mut socket: mio::net::TcpStream, addr: SocketAddr) {
-    let mut sess = SessionHandler::new(socket, addr);
+    let mut sess = SessionHandler::new(socket, addr, true);
     sess.send_session_identification();
     sess.session_loop();
 }
