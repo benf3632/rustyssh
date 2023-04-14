e1d3d88fa0e6aabab6539a97512ffa5ae14272d6
benf3632 <benf3632@gmail.com>
2023-04-15 01:09:30 +0300

    modify algo to support dynamic ciphers

--- rustyssh/src/algo.rs
+++ rustyssh/src/algo.rs
@@ -1,15 +1,16 @@
+use crate::crypto::cipher::{CipherMode, AES_256_GCM};
 use crate::crypto::kex::dh_groups::DH_P_14;
 use crate::signkey::SignatureType;
 use crate::sshbuffer::SSHBuffer;
 
-pub struct AlgoType {
+pub struct AlgoType<'a> {
     pub name: &'static str,
     pub usable: bool,
-    pub kex: Option<&'static Kex>,
+    pub kex: Option<&'a Kex>,
     // cipher_mode: CipherMode,
     pub sig_type: Option<SignatureType>,
-    pub hash: Option<&'static Hash>,
-    pub cipher: Option<&'static Cipher>,
+    pub hash: Option<&'a Hash>,
+    pub cipher: Option<&'static CipherMode>,
 }
 
 pub enum KexMode {
@@ -18,12 +19,17 @@ pub enum KexMode {
     // CURVE25519,
 }
 
-pub struct Kex {
-    pub mode: KexMode,
-    pub digest: &'static ring::digest::Algorithm,
+pub enum Digest {
+    SHA256,
+    SHA1,
 }
 
-pub static SSH_NOCOMPRESS: [AlgoType; 1] = [AlgoType {
+pub struct Kex {
+    pub mode: KexMode,
+    pub digest: Digest,
+}
+
+pub const SSH_NOCOMPRESS: [AlgoType; 1] = [AlgoType {
     name: "none",
     usable: true,
     kex: None,
@@ -33,17 +39,17 @@ pub static SSH_NOCOMPRESS: [AlgoType; 1] = [AlgoType {
 }];
 
 const KEX_DH_GROUP14_MODE: KexMode = KexMode::NormalDH(&DH_P_14);
-static KEX_DH_GROUP14_SHA256: Kex = Kex {
+const KEX_DH_GROUP14_SHA256: Kex = Kex {
     mode: KEX_DH_GROUP14_MODE,
-    digest: &ring::digest::SHA256,
+    digest: Digest::SHA256,
 };
 
-static KEX_DH_GROUP14_SHA1: Kex = Kex {
+const KEX_DH_GROUP14_SHA1: Kex = Kex {
     mode: KEX_DH_GROUP14_MODE,
-    digest: &ring::digest::SHA1_FOR_LEGACY_USE_ONLY,
+    digest: Digest::SHA1,
 };
 
-pub static SSHKEX: [AlgoType; 2] = [
+pub const SSHKEX: [AlgoType; 2] = [
     // TODO: don't forget to add required algos
     AlgoType {
         name: "diffie-hellman-group14-sha256",
@@ -63,7 +69,7 @@ pub static SSHKEX: [AlgoType; 2] = [
     },
 ];
 
-pub static SIGALGS: [AlgoType; 1] = [AlgoType {
+pub const SIGALGS: [AlgoType; 1] = [AlgoType {
     name: "ssh-rsa",
     usable: true,
     kex: None,
@@ -73,18 +79,18 @@ pub static SIGALGS: [AlgoType; 1] = [AlgoType {
 }];
 
 pub struct Hash {
-    pub digest: &'static ring::digest::Algorithm,
+    pub digest: Digest,
     pub keysize: u64,
     pub hashsize: u8,
 }
 
-static SHA2_256: Hash = Hash {
-    digest: &ring::digest::SHA256,
+const SHA2_256: Hash = Hash {
+    digest: Digest::SHA256,
     keysize: 32,
     hashsize: 32,
 };
 
-pub static SSHHASHES: [AlgoType; 1] = [AlgoType {
+pub const SSHHASHES: [AlgoType; 1] = [AlgoType {
     name: "hmac-sha2-256",
     usable: true,
     sig_type: None,
@@ -93,19 +99,7 @@ pub static SSHHASHES: [AlgoType; 1] = [AlgoType {
     cipher: None,
 }];
 
-pub struct Cipher {
-    pub cipher_type: &'static ring::aead::Algorithm,
-    pub keysize: u64,
-    pub blocksize: u8,
-}
-
-static AES_256_GCM: Cipher = Cipher {
-    cipher_type: &ring::aead::AES_256_GCM,
-    keysize: 32,
-    blocksize: 16,
-};
-
-pub static SSHCIPHERS: [AlgoType; 1] = [AlgoType {
+pub const SSHCIPHERS: [AlgoType; 1] = [AlgoType {
     name: "aes256-gcm@openssh.com",
     usable: true,
     sig_type: None,
