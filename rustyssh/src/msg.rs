9dcdec9fa73d0429f26e94673b6b205385d08baf
benf3632 <benf3632@gmail.com>
2023-04-16 00:16:31 +0300

    impl `from_u8`

--- rustyssh/src/msg.rs
+++ rustyssh/src/msg.rs
@@ -1,3 +1,7 @@
+use core::panic;
+
+#[derive(Debug)]
+#[repr(u8)]
 pub enum SSHMsg {
     None = 0,
     // transport layer protocol messages refer to https://www.rfc-editor.org/rfc/rfc4253
@@ -8,4 +12,18 @@ pub enum SSHMsg {
     // alogrithem negotiation
     KEXINIT = 20,
     NEWKEYS = 21,
+}
+
+impl SSHMsg {
+    pub fn from_u8(value: u8) -> SSHMsg {
+        match value {
+            0 => SSHMsg::None,
+            1 => SSHMsg::DISCONNECT,
+            2 => SSHMsg::IGNORE,
+            3 => SSHMsg::UNIMPLEMENTED,
+            20 => SSHMsg::KEXINIT,
+            21 => SSHMsg::NEWKEYS,
+            _ => panic!("Unimplemented msg: {}", value),
+        }
+    }
 }
