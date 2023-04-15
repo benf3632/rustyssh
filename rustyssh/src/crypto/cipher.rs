e9a18ef640476d202653bc782103981e84adb9c3
benf3632 <benf3632@gmail.com>
2023-04-15 21:57:01 +0300

    add `Crypt` trait

--- rustyssh/src/crypto/cipher.rs
+++ rustyssh/src/crypto/cipher.rs
@@ -1,5 +1,7 @@
 use ring::error;
 
+use crate::algo::Hash;
+
 use self::{
     aes_gcm::{new_aes_gcm_128, new_aes_gcm_256},
     none::new_none_cipher,
@@ -45,21 +47,39 @@ pub enum Direction {
 
 pub trait Crypt {
     fn init(&mut self, key: &[u8], iv: &[u8]) -> Result<(), error::Unspecified>;
+
     fn encrypt(
         &mut self,
         plaintext: &[u8],
         ciphertext: &mut [u8],
     ) -> Result<(), error::Unspecified>;
+
+    fn encrypt_in_place(&mut self, plaintext: &mut [u8]) -> Result<(), error::Unspecified>;
+
     fn decrypt(
         &mut self,
         ciphertext: &[u8],
         plaintext: &mut [u8],
     ) -> Result<(), error::Unspecified>;
+
+    fn decrypt_in_place(&mut self, ciphertext: &mut [u8]) -> Result<(), error::Unspecified>;
+
     fn aead_crypt(
         &mut self,
         input: &[u8],
         output: &mut [u8],
         direction: Direction,
     ) -> Result<(), error::Unspecified>;
-    fn is_aead(&mut self) -> bool;
+
+    fn aead_crypt_in_place(
+        &mut self,
+        input: &mut [u8],
+        direction: Direction,
+    ) -> Result<(), error::Unspecified>;
+
+    fn aead_getlength(&mut self, input: &[u8]) -> Result<u32, error::Unspecified>;
+
+    fn aead_mac(&self) -> Hash;
+
+    fn is_aead(&self) -> bool;
 }
