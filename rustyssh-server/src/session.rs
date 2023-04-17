2277bbefa6141fa9a88be3e2ac5941aea5dcfee2
benf3632 <benf3632@gmail.com>
2023-04-17 21:21:06 +0300

    remove warning about mut

--- rustyssh-server/src/session.rs
+++ rustyssh-server/src/session.rs
@@ -2,7 +2,7 @@ use std::net::SocketAddr;
 
 use rustyssh::session::SessionHandler;
 
-pub fn handle_connection(mut socket: mio::net::TcpStream, addr: SocketAddr) {
+pub fn handle_connection(socket: mio::net::TcpStream, addr: SocketAddr) {
     let mut sess = SessionHandler::new(socket, addr, true);
     sess.send_session_identification();
     sess.session_loop();
