ce4886d6222c5ecbca359303f8384c39022540e1
benf3632 <benf3632@gmail.com>
2023-04-17 21:19:22 +0300

    change name to uppercase for const values

--- rustyssh/src/server/session.rs
+++ rustyssh/src/server/session.rs
@@ -1,8 +1,9 @@
 use crate::kex::recv_msg_kexinit;
 use crate::msg::SSHMsg;
 use crate::packet::PacketType;
+use crate::session::Session;
 
-pub const server_packettypes: [PacketType; 1] = [PacketType {
+pub const SERVER_PACKET_TYPES: [PacketType; 1] = [PacketType {
     msg_type: SSHMsg::KEXINIT,
     handler: &recv_msg_kexinit,
 }];
