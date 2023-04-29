862c6c3a36cfc1d00ca45d7e307c2d8587d3fa44
benf3632 <benf3632@gmail.com>
2023-04-29 23:44:08 +0300

    impl read_kexinit_algos

--- rustyssh/src/kex.rs
+++ rustyssh/src/kex.rs
@@ -4,7 +4,7 @@ use std::time::Instant;
 
 use crate::{
     msg::SSHMsg,
-    namelist::{CIPHER_ORDER, COMPRESSION_ORDER, HMAC_ORDER, KEX_ORDER, SIGNATURE_ORDER},
+    namelist::{Name, CIPHER_ORDER, COMPRESSION_ORDER, HMAC_ORDER, KEX_ORDER, SIGNATURE_ORDER},
     session::SessionHandler,
 };
 
@@ -40,17 +40,106 @@ impl Default for KexState {
     }
 }
 
+fn match_algo<'a>(local_algos: &'a [Name], remote_algos: &Vec<u8>) -> Option<&'a Name> {
+    let remote_algos = std::str::from_utf8(&remote_algos).expect("Invalid namelist");
+    let remote_algos_list = remote_algos.split(",");
+
+    for remote_algo_name in remote_algos_list {
+        for local_algo_name in local_algos {
+            if local_algo_name == remote_algo_name {
+                return Some(local_algo_name);
+            }
+        }
+    }
+    None
+}
+
 impl SessionHandler {
     pub fn recv_msg_kexinit(&mut self) {
         trace!("enter recv_msg_kexinit");
+
         if !self.session.kex_state.sent_kex_init {
             debug!("Sending KEXINIT");
             self.send_msg_kexinit();
         }
 
-        debug!("payload: {:?}", self.session.payload);
+        if self.session.kex_state.recv_kex_init {
+            panic!("Recieved already kexinit message");
+        }
+
+        self.read_kexinit_algos();
+
+        self.session.kex_state.recv_kex_init = true;
 
         trace!("exit recv_msg_kexinit");
+    }
+
+    pub fn read_kexinit_algos(&mut self) {
+        let payload = self.session.payload.as_mut().expect("Expected payload");
+
+        // skip cookie, only used for the hash
+        payload.incr_pos(COOKIE_LEN);
+
+        // match kex algos
+        let (kex_algos, _) = payload.get_string();
+        let kex_name_match =
+            match_algo(KEX_ORDER, &kex_algos).expect("No matching kex algorithms found");
+        debug!("KEX: {:?}", kex_name_match);
+
+        // match server host key algos
+        let (server_host_algos, _) = payload.get_string();
+        let server_host_match = match_algo(SIGNATURE_ORDER, &server_host_algos)
+            .expect("No matching server host keys algorithms found");
+        debug!("SERVER_HOST_KEY: {:?}", server_host_match);
+
+        // match ciphers client to server algos
+        let (ciphers_c2s_algos, _) = payload.get_string();
+        let ciphers_c2s_match = match_algo(CIPHER_ORDER, &ciphers_c2s_algos)
+            .expect("No matching encryption ctos algorithms found");
+        debug!("CIPHER_C2S: {:?}", ciphers_c2s_match);
+
+        // match ciphers server to client algos
+        let (ciphers_s2c_algos, _) = payload.get_string();
+        let ciphers_s2c_match = match_algo(CIPHER_ORDER, &ciphers_s2c_algos)
+            .expect("No matching encryption ctos algorithms found");
+        debug!("CIPHER_S2C: {:?}", ciphers_s2c_match);
+
+        // match mac client to server algos
+        let (macs_c2s_algos, _) = payload.get_string();
+        let macs_c2s_match = match_algo(HMAC_ORDER, &macs_c2s_algos)
+            .expect("No matching macs ctos algorithms found");
+        debug!("MAC_C2S: {:?}", macs_c2s_match);
+
+        // match mac server to client algos
+        let (macs_s2c_algos, _) = payload.get_string();
+        let macs_s2c_match = match_algo(HMAC_ORDER, &macs_s2c_algos)
+            .expect("No matching macs ctos algorithms found");
+        debug!("MAC_S2C: {:?}", macs_s2c_match);
+
+        // match compression client to server algos
+        let (compression_c2s_algos, _) = payload.get_string();
+        let compression_c2s_match = match_algo(COMPRESSION_ORDER, &compression_c2s_algos)
+            .expect("No matching compression ctos algorithms found");
+        debug!("COMPRESSION_C2S: {:?}", compression_c2s_match);
+
+        // match compression server to client algos
+        let (compression_s2c_algos, _) = payload.get_string();
+        let compression_s2c_match = match_algo(COMPRESSION_ORDER, &compression_s2c_algos)
+            .expect("No matching compression ctos algorithms found");
+        debug!("COMPRESSION_S2C: {:?}", compression_s2c_match);
+
+        // skip languages namelist
+        payload.get_string();
+        payload.get_string();
+
+        let is_first_kex_packet_follows = payload.get_bool();
+        self.session.kex_state.them_first_follow = is_first_kex_packet_follows;
+        if is_first_kex_packet_follows {
+            self.session.ignore_next = true;
+        }
+
+        // reserved for futurue extensions
+        payload.get_int();
     }
 
     pub fn send_msg_kexinit(&mut self) {
@@ -108,7 +197,6 @@ impl SessionHandler {
             .expect("Failed to encrypt payload");
 
         packet.set_pos(0);
-        debug!("{:?}", packet);
 
         self.packet_handler.enqueue_packet(packet);
 
