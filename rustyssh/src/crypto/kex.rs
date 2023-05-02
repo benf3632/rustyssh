088265144fa50967901df7d713fe6226301d9a21
benf3632 <benf3632@gmail.com>
2023-05-02 12:09:15 +0300

    add KexMode

--- rustyssh/src/crypto/kex.rs
+++ rustyssh/src/crypto/kex.rs
@@ -1,10 +1,46 @@
+use ring::digest::{Algorithm, SHA1_FOR_LEGACY_USE_ONLY, SHA256};
+use std::collections::HashMap;
+
+use once_cell::sync::Lazy;
+
+use crate::namelist::Name;
+
+use self::dh::{Dh, DH_GROUP14};
+
 pub mod dh;
 pub mod dh_groups;
 
-pub enum KexMode {
-    DH,
+#[derive(Clone)]
+pub enum KexType {
+    DH(&'static Dh),
     ECDH,
 }
+
+#[derive(Clone)]
+pub struct KexMode {
+    pub kex_type: KexType,
+    pub digest: Option<&'static Algorithm>,
+}
+
+pub static DH_GROUP14_SHA256_MODE: KexMode = KexMode {
+    kex_type: KexType::DH(&DH_GROUP14),
+    digest: Some(&SHA256),
+};
+
+pub static DH_GROUP14_SHA1_MODE: KexMode = KexMode {
+    kex_type: KexType::DH(&DH_GROUP14),
+    digest: Some(&SHA1_FOR_LEGACY_USE_ONLY),
+};
+
+pub const KEX_DH_GROUP14_SHA256: Name = Name("diffie-hellman-group14-sha256");
+pub const KEX_DH_GROUP14_SHA1: Name = Name("diffie-hellman-group14-sha1");
+
+pub static KEXS: Lazy<HashMap<&'static Name, &'static KexMode>> = Lazy::new(|| {
+    let mut h: HashMap<&'static Name, &'static KexMode> = HashMap::new();
+    h.insert(&KEX_DH_GROUP14_SHA256, &DH_GROUP14_SHA256_MODE);
+    h.insert(&KEX_DH_GROUP14_SHA1, &DH_GROUP14_SHA1_MODE);
+    h
+});
 
 pub trait Kex {
     fn generate_public_key(&mut self) -> Vec<u8>;
--- rustyssh/src/crypto/kex/dh.rs
+++ rustyssh/src/crypto/kex/dh.rs
@@ -2,19 +2,39 @@ use std::ops::{Div, Sub};
 
 use num_bigint::{BigUint, RandBigInt};
 
-use super::Kex;
+use super::{dh_groups::DH_P_14, Kex};
 
+#[derive(Clone)]
 pub struct Dh {
-    pub prime: BigUint,
+    pub prime: &'static [u8],
     pub private_key: Option<BigUint>,
     pub public_key: Option<BigUint>,
-    pub generator: BigUint,
+    pub generator: &'static [u8],
 }
+
+// impl Clone for Dh {
+//     fn clone(&self) -> Self {
+//         Self {
+//             prime: self.prime.clone(),
+//             private_key: self.private_key.clone(),
+//             public_key: self.public_key.clone(),
+//             generator: self.generator.clone(),
+//         }
+//     }
+// }
+
+pub static DH_GROUP14: Dh = Dh {
+    prime: &DH_P_14,
+    generator: &[2],
+    private_key: None,
+    public_key: None,
+};
 
 impl Dh {
     pub fn generate_private_key(&mut self) {
         // calulcate q = (p - 1) / 2
-        let prime_minus_one: BigUint = (&self.prime).sub(BigUint::new(vec![1]));
+        let prime = BigUint::from_bytes_be(self.prime);
+        let prime_minus_one: BigUint = (&prime).sub(BigUint::new(vec![1]));
         let q: BigUint = prime_minus_one.div(BigUint::new(vec![2]));
         self.private_key = Some(rand::thread_rng().gen_biguint_range(&BigUint::new(vec![0]), &q));
     }
@@ -23,18 +43,21 @@ impl Dh {
 impl Kex for Dh {
     fn generate_public_key(&mut self) -> Vec<u8> {
         self.generate_private_key();
-        self.public_key = Some(self.generator.modpow(
+        let generator = BigUint::from_bytes_be(self.generator);
+        let prime = BigUint::from_bytes_be(self.prime);
+        self.public_key = Some(generator.modpow(
             self.private_key.as_ref().expect("private key should exist"),
-            &self.prime,
+            &prime,
         ));
         self.public_key.as_ref().unwrap().to_bytes_be()
     }
     fn generate_secret_key(&mut self, public_key: &[u8]) -> Vec<u8> {
+        let prime = BigUint::from_bytes_be(self.prime);
         let public_key = BigUint::from_bytes_be(public_key);
         public_key
             .modpow(
                 &self.private_key.as_ref().expect("private key should exist"),
-                &self.prime,
+                &prime,
             )
             .to_bytes_be()
     }
