5dc20cd8e42be7cb0c11ef4ca7a680073a5d3d79
benf3632 <benf3632@gmail.com>
2023-05-13 17:17:17 +0300

    add recv new keys handler

--- rustyssh/src/server/session.rs
+++ rustyssh/src/server/session.rs
@@ -11,5 +11,6 @@ pub static SERVER_PACKET_HANDLERS: Lazy<
     let mut h: HashMap<SSHMsg, &(dyn Fn(&mut SessionHandler) + Send + Sync)> = HashMap::new();
     h.insert(SSHMsg::KEXINIT, &SessionHandler::recv_msg_kexinit);
     h.insert(SSHMsg::KEXDHINIT, &SessionHandler::recv_msg_kex_dh_init);
+    h.insert(SSHMsg::NEWKEYS, &SessionHandler::recv_msg_kex_newkeys);
     h
 });
